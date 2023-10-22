102b58a2d4bc9983da21c9b6f06b055efc533e3e
Brownjur <117828536+KiraHigashikata@users.noreply.github.com>
2023-10-22 13:02:16 +0000

    fr

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ const content = {
 
     sammfunsEffekt: "Diktaturet i Egypt hadde dype konsekvenser for det egyptiske samfunnet. Mange opplevde politisk forfølgelse og fengsling for å ha uttrykt sine meninger eller deltatt i fredelige protester. Ytringsfriheten ble sterkt begrenset, og media var underlagt streng sensur. Dette resulterte i en fryktkultur der folk var redde for å si noe som kunne utfordre regimets autoritet. Økonomisk ulikhet og korrupsjon blomstret, og mange egyptere levde i fattigdom og undertrykkelse.",
 
-    håp: "Til tross for undertrykkelsen har egyptiske borgere aldri gitt opp håpet om frihet og demokrati. Gjennom årene har det vært flere opprør og revolusjoner som har forsøkt å avsette det autoritære regimet. Den mest kjente var den egyptiske revolusjonen i 2011, også kjent som 'Arabisk vår' Tusenvis av mennesker gikk ut i gatene og protesterte mot Mubarak-regimet, og krevde demokratiske reformer og respekt for menneskerettighetene.\nSelv om revolusjonen førte til at Mubarak ble avsatt, har veien mot demokrati vært vanskeligere enn mange hadde håpet på. Etter et kort mellomspill med demokratisk valg, kom Egypt under militærstyre igjen i 2013. Siden da har landet vært preget av undertrykkelse, fengsling av politiske motstandere og begrensning av ytringsfriheten.",
+    håp: "Til tross for undertrykkelsen har egyptiske borgere aldri gitt opp håpet om frihet og demokrati. Gjennom årene har det vært flere opprør og revolusjoner som har forsøkt å avsette det autoritære regimet. Den mest kjente var den egyptiske revolusjonen i 2011, også kjent som 'Arabisk vår' Tusenvis av mennesker gikk ut i gatene og protesterte mot Mubarak-regimet, og krevde demokratiske reformer og respekt for menneskerettighetene.\n\nSelv om revolusjonen førte til at Mubarak ble avsatt, har veien mot demokrati vært vanskeligere enn mange hadde håpet på. Etter et kort mellomspill med demokratisk valg, kom Egypt under militærstyre igjen i 2013. Siden da har landet vært preget av undertrykkelse, fengsling av politiske motstandere og begrensning av ytringsfriheten.",
 
     konklusjon: "Diktaturet i Egypt har vært et dystert kapittel i landets historie. Undertrykkelse, maktmisbruk og begrensning av friheter har preget det egyptiske samfunnet i flere tiår. Likevel er det håp i befolkningens kamp for frihet og demokrati. Egyptiske borgere har vist en utrettelig vilje til å kjempe for sine rettigheter og motstå undertrykkelse. Det er avgjørende at verdenssamfunnet støtter deres kamp og arbeider for å sikre et demokratisk Egypt der individuelle friheter og menneskerettigheter blir respektert.",
 
-    norge: "Diktaturet i Egypt har vært et dystert kapittel i landets historie, preget av undertrykkelse, fengsling av politiske motstandere og begrensning av ytringsfriheten. Befolkningen har likevel vist en utrettelig vilje til å kjempe for frihet og demokrati. Det er viktig at verdenssamfunnet støtter deres kamp og arbeider for å sikre et demokratisk Egypt der individuelle friheter og menneskerettigheter blir respektert.\nSammenlignet med Egypt har Norge en helt annen politisk virkelighet. Norge har en lang tradisjon med demokrati og respekt for menneskerettigheter. Landet har et parlamentarisk konstitusjonelt monarki, der makten er delt mellom kongen, regjeringen og Stortinget. Dette systemet gir plass til politiske partier, ytringsfrihet og en sterk rettsstat.\nEgypt, derimot, har opplevd perioder med autoritært styre. Fra 1952 til 2011 var landet under militærstyre, først med Gamal Abdel Nasser, deretter Anwar Sadat og til slutt Hosni Mubarak. Disse lederne begrenset ytringsfriheten, undertrykte politiske motstandere og dominerte alle aspekter av samfunnet.\nSelv om Norge og Egypt begge har opplevd perioder med autoritært styre, er det viktige forskjeller. Norge har alltid hatt en sterk demokratisk tradisjon, der politiske rettigheter og individuell frihet er sentrale verdier. I Egypt har demokratiet vært mer skjørt, og befolkningen har måttet kjempe for å oppnå og bevare sine rettigheter.",
+    norge: "Diktaturet i Egypt har vært et dystert kapittel i landets historie, preget av undertrykkelse, fengsling av politiske motstandere og begrensning av ytringsfriheten. Befolkningen har likevel vist en utrettelig vilje til å kjempe for frihet og demokrati. Det er viktig at verdenssamfunnet støtter deres kamp og arbeider for å sikre et demokratisk Egypt der individuelle friheter og menneskerettigheter blir respektert.\n\nSammenlignet med Egypt har Norge en helt annen politisk virkelighet. Norge har en lang tradisjon med demokrati og respekt for menneskerettigheter. Landet har et parlamentarisk konstitusjonelt monarki, der makten er delt mellom kongen, regjeringen og Stortinget. Dette systemet gir plass til politiske partier, ytringsfrihet og en sterk rettsstat.\n\nEgypt, derimot, har opplevd perioder med autoritært styre. Fra 1952 til 2011 var landet under militærstyre, først med Gamal Abdel Nasser, deretter Anwar Sadat og til slutt Hosni Mubarak. Disse lederne begrenset ytringsfriheten, undertrykte politiske motstandere og dominerte alle aspekter av samfunnet.\n\nSelv om Norge og Egypt begge har opplevd perioder med autoritært styre, er det viktige forskjeller. Norge har alltid hatt en sterk demokratisk tradisjon, der politiske rettigheter og individuell frihet er sentrale verdier. I Egypt har demokratiet vært mer skjørt, og befolkningen har måttet kjempe for å oppnå og bevare sine rettigheter.",
 
     kilder: [
         {
@@ -38,26 +38,23 @@ const content = {
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    var screenHeight = window.innerHeight;
     
-
+    
     var tittel = document.getElementById('title');
     var intro = document.getElementById('intro');
     var opprinnelse = document.getElementById('origin');
     var samfunn = document.getElementById('societal');
     var hope = document.getElementById('hope');
-    var norge = document.getElementById('noorway');
+    var norge = document.getElementById('norway');
     var avsluttning = document.getElementById('summary');
     var footer = document.getElementById('footer');
-
+    
     var hero = document.getElementById('hero');
-    var style = hero.style;
 
-    style.height = screenHeight;
 
     tittel.innerHTML = content.tittel;
     intro.innerHTML = content.intro;
-    opprinnelse.innerHTML = content.opprinnelse;
+    opprinnelse.innerHTML = content.opprinelse;
     samfunn.innerHTML = content.sammfunsEffekt;
     hope.innerHTML = content.håp;
     norge.innerHTML = content.norge;
